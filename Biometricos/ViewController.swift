2dac8d7c26c7827fa3085215394f54061d01a840
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2022-11-22 11:17:32 -0600

    Valida si la autenticacion es faceid o touch id , permiso de face id agregado

--- Biometricos/ViewController.swift
+++ Biometricos/ViewController.swift
@@ -9,10 +9,41 @@ import UIKit
 import LocalAuthentication
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var faceOrTouch: UIImageView!
+    
+    let contexto = LAContext()
+    var error: NSError? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if contexto.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            evaluateBiometryType()
+        }
+    }
+    
+    func evaluateBiometryType() {
+        switch contexto.biometryType {
+        case .faceID:
+            print("Face ID")
+            faceOrTouch.image = UIImage(named: "face")
+        case .touchID:
+            print("Touch ID")
+            faceOrTouch.image = UIImage(named: "touch")
+        case .none:
+            print("No esta configurado")
+            print("Iniciar sesion con contraseña")
+                
+        default:
+            print("Desconocido")
+        }
     }
 
     @IBAction func autenticarButton(_ sender: Any) {
@@ -20,8 +51,6 @@ class ViewController: UIViewController {
     }
     
     func biometrics() {
-        let contexto = LAContext()
-        var error: NSError? = nil
         if contexto.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
             let reasen = "Por favor autoriza el inicio de sesion con touchID o FaceID"
             contexto.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reasen) { succes, error in
